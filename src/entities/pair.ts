cb0a24a0147b6c03d1dde986eca856140bb3e8f4
Tien Nam Dao <tien.dao2@tiki.vn>
2022-08-15 11:37:04 +0700

    fix: symbol+name token pair

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -56,8 +56,8 @@ export class Pair {
       tokenAmounts[0].token.chainId,
       Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token),
       18,
-      'Cake-LP',
-      'Astra LPs'
+      'WASA-LP',
+      'Solarswap LPs'
     )
     this.tokenAmounts = tokenAmounts as [TokenAmount, TokenAmount]
   }
